8da51dacc0d4d5fce985e7a98ca71b432b2df507
Jonghyun-Joo <jojo@monymony.co>
2024-11-03 16:49:12 +0900

    temp

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthGuard } from "@nestjs/passport";
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+  
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req: any) {
+    return req;
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { GoogleStrategy } from "./google/google.strategy";
+import { AuthService } from "./auth.service";
 
 @Module({
   imports: [],
+  providers: [AuthService, GoogleStrategy],
 })
 export class AuthModule {}
